be2b08eef5dfee9d3848f9fb46b5d9186b5c5a0d
SMJin <sstaw@naver.com>
2021-07-10 22:34:50 +0900

    solve calculator

--- basic-tsc-quiz/calculator.ts
+++ basic-tsc-quiz/calculator.ts
@@ -1,6 +1,24 @@
 /**
  * Let's make a calculator 🧮
  */
+
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+
+function calculate(command: Command, x: number, y: number): number {
+    switch (command) {
+        case 'add':
+            return x + y;
+        case 'substract':
+            return x - y;
+        case 'multiply':
+            return x * y;
+        case 'divide':
+            return x / y;
+        case 'remainder':
+            return x % y;
+    }
+}
+
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
