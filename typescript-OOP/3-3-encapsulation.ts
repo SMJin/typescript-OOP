e8b8446492d72f3b6b9690fac7150e8bbe2851f5
SMJin <sstaw@naver.com>
2021-07-11 00:00:49 +0900

    correct encapsultaion + getter setter with User Class

--- typescript-OOP/3-3-encapsulation.ts
+++ typescript-OOP/3-3-encapsulation.ts
@@ -4,6 +4,9 @@
     hasMilk: boolean;
   };
 
+  // public
+  // private
+  // protected
   class CoffeeMaker {
     private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
     private coffeeBeans: number = 0; // instance (object) level
@@ -12,11 +15,18 @@
       this.coffeeBeans = coffeeBeans;
     }
 
-    static makeMachine(coffeeBeans: number, shots: number): CoffeeCup {
-      return new CoffeeMaker(coffeeBeans).makeCoffee(shots);
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
     }
 
-    private makeCoffee(shots: number): CoffeeCup {
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
       if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
         throw new Error('Not enough coffee beans!');
       }
@@ -28,72 +38,25 @@
     }
   }
 
-  const maker = CoffeeMaker.makeMachine(32, 2);
-  console.log(maker);
-  const maker2 = CoffeeMaker.makeMachine(14, 1);
-  console.log(maker2);
+  const maker = CoffeeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+
+  class User {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num < 0) {
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, public lastName: string) {}
+  }
+  const user = new User('Steve', 'Jobs');
+  user.age = 6;
+  console.log(user.fullName);
 }
-
-
-// {
-//   type CoffeeCup = {
-//     shots: number;
-//     hasMilk: boolean;
-//   };
-
-//   // public
-//   // private
-//   // protected
-//   class CoffeeMaker {
-//     private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-//     private coffeeBeans: number = 0; // instance (object) level
-
-//     private constructor(coffeeBeans: number) {
-//       this.coffeeBeans = coffeeBeans;
-//     }
-
-//     static makeMachine(coffeeBeans: number): CoffeeMaker {
-//       return new CoffeeMaker(coffeeBeans);
-//     }
-
-//     fillCoffeeBeans(beans: number) {
-//       if (beans < 0) {
-//         throw new Error('value for beans should be greater than 0');
-//       }
-//       this.coffeeBeans += beans;
-//     }
-
-//     makeCoffee(shots: number): CoffeeCup {
-//       if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-//         throw new Error('Not enough coffee beans!');
-//       }
-//       this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
-//       return {
-//         shots,
-//         hasMilk: false,
-//       };
-//     }
-//   }
-
-//   const maker = CoffeeMaker.makeMachine(32);
-//   maker.fillCoffeeBeans(32);
-
-//   class User {
-//     get fullName(): string {
-//       return `${this.firstName} ${this.lastName}`;
-//     }
-//     private internalAge = 4;
-//     get age(): number {
-//       return this.internalAge;
-//     }
-//     set age(num: number) {
-//       if (num < 0) {
-//       }
-//       this.internalAge = num;
-//     }
-//     constructor(private firstName: string, public lastName: string) {}
-//   }
-//   const user = new User('Steve', 'Jobs');
-//   user.age = 6;
-//   console.log(user.fullName);
-// }
