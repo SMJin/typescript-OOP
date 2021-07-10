e2be833ce9044c949434b51909c48f081498924f
SMJin <sstaw@naver.com>
2021-07-10 22:54:27 +0900

    solve loading

--- basic-tsc-quiz/loading.ts
+++ basic-tsc-quiz/loading.ts
@@ -19,6 +19,21 @@
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
+  function printLoginState(correntState: ResourceLoadState) {
+    switch(correntState.state) {
+      case 'loading':
+        console.log(`👀 loading...`);
+        break;
+        case 'success':
+          console.log(`😃 loaded`);
+          break;
+        case 'fail':
+          console.log(`😱 no network`);
+          break;
+        default:
+          throw Error('404: unknown Error !');
+    }
+  }
 
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
