4d178a8578d5d9d944873a316de4d095f2d8061a
SMJin <sstaw@naver.com>
2021-07-10 23:55:28 +0900

    solve encapsulation

--- typescript-OOP/3-2-class.ts
+++ typescript-OOP/3-2-class.ts
@@ -1,72 +1,37 @@
 {
-
   type CoffeeCup = {
     shots: number;
     hasMilk: boolean;
-  }
+  };
 
-  class CoffeeMachine {
-    static BEANS_GRAMM_PER_SHOT: number = 7; 
-    coffeeBeans: number = 0;
-    
+  class CoffeeMaker {
+    static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    coffeeBeans: number = 0; // instance (object) level
+
     constructor(coffeeBeans: number) {
       this.coffeeBeans = coffeeBeans;
     }
 
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
     makeCoffee(shots: number): CoffeeCup {
-      if (shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT > this.coffeeBeans) {
-        throw Error('not enough coffee beans !');
-      } else {
-        console.log(`Making coffee with ${shots} shots ...`);
-        this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
-        return {
-          shots: shots,
-          hasMilk: false
-        }
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
       }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
     }
   }
 
-  const coffeeMachine = new CoffeeMachine(20);
-  coffeeMachine.makeCoffee(2);
-  console.log(coffeeMachine);
-  
+  const maker = new CoffeeMaker(32);
+  console.log(maker);
+  const maker2 = new CoffeeMaker(14);
+  console.log(maker2);
+
+  const maker3 = CoffeeMaker.makeMachine(3);
 }
-
-// {
-//   type CoffeeCup = {
-//     shots: number;
-//     hasMilk: boolean;
-//   };
-
-//   class CoffeeMaker {
-//     static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-//     coffeeBeans: number = 0; // instance (object) level
-
-//     constructor(coffeeBeans: number) {
-//       this.coffeeBeans = coffeeBeans;
-//     }
-
-//     static makeMachine(coffeeBeans: number): CoffeeMaker {
-//       return new CoffeeMaker(coffeeBeans);
-//     }
-
-//     makeCoffee(shots: number): CoffeeCup {
-//       if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-//         throw new Error('Not enough coffee beans!');
-//       }
-//       this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
-//       return {
-//         shots,
-//         hasMilk: false,
-//       };
-//     }
-//   }
-
-//   const maker = new CoffeeMaker(32);
-//   console.log(maker);
-//   const maker2 = new CoffeeMaker(14);
-//   console.log(maker2);
-
-//   const maker3 = CoffeeMaker.makeMachine(3);
-// }
