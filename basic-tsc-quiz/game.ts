f8aa1786d022a10532a0e13fe7b92ad409b9608c
SMJin <sstaw@naver.com>
2021-07-10 22:44:33 +0900

    solve game

--- basic-tsc-quiz/game.ts
+++ basic-tsc-quiz/game.ts
@@ -1,6 +1,35 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+    x: number,
+    y: number
+}
+const position: Position = {
+    x: 0,
+    y: 0
+}
+
+type Joystick = 'up' | 'down' | 'left' | 'right';
+function move(joystick: Joystick) {
+    switch(joystick) {
+        case 'up':
+            position.y ++;
+            break;
+        case 'down':
+            position.y --;
+            break;
+        case 'left':
+            position.x --;
+            break;
+        case 'right':
+            position.x ++;
+            break;
+        default:
+            throw Error('missing position ...');
+    }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
